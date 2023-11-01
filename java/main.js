5ac040b7079b90569422b91f93e60b62e5e8beb4
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-01 20:00:15 +0100

    1.4

--- java/main.js
+++ java/main.js
@@ -1,38 +1,40 @@
-/* console.log(mails[3]) */
+
 let btn=document.getElementById("submit")
 const output=document.getElementById("output");
 output.style.backgroundColor="grey"
-
-
 const mails=["giulio@mail","franco@mail","claudio@mail","andrea@mail","alessio@mail","marco@mail","sandro@mail","lucia@mail","alessia@mail","francesca@mail"];
 
 btn.addEventListener("click",function(){
     
     let emailIns=document.getElementById("input").value;
+    
     console.log(emailIns);
+    console.log(mails);
+
     output.style.padding="10px";
     output.style.borderRadius="10px"
-
     
     if(emailIns==""){
         output.innerHTML=("Non hai inserito la tua e-mail")
     }
     else{
-    
-        for (let i =0 ; i < mails.length; i++) {
+        
+        for (let i =0 ; i <= mails.length; i++) {
             let Elementmail=mails[i];
             
-            if(emailIns=Elementmail){
+            if(emailIns==Elementmail){
                 output.innerHTML=("la tua e-mail già esiste!");
                 break;
             }
-    
-            else if(!(emailIns=Elementmail)){}
-            output.innerHTML=("La tua e-mail è valida,benvenuto!");
-            break;
+            
+            else if(!(emailIns==Elementmail)){
+                output.innerHTML=("La tua e-mail è valida,benvenuto!");
+                break;
+            } 
+            
         }
     }
-
+    
 
 })
 
