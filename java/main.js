030fe927218547c6d7229b2c13282f255b2a830b
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-01 19:41:42 +0100

    1.1

---
+++ java/main.js
@@ -0,0 +1,66 @@
+/* console.log(mails[3]) */
+let btn=document.getElementById("submit")
+const output=document.getElementById("output");
+output.style.backgroundColor="grey"
+
+
+const mails=["giulio@mail","franco@mail","claudio@mail","andrea@mail","alessio@mail","marco@mail","sandro@mail","lucia@mail","alessia@mail","francesca@mail"];
+
+btn.addEventListener("click",function(){
+    
+    let emailIns=document.getElementById("input").value;
+    console.log(emailIns);
+    output.style.padding="10px";
+    output.style.borderRadius="10px"
+
+    
+    if(emailIns==""){
+        output.innerHTML=("Non hai inserito la tua e-mail")
+    }
+    else{
+    
+        for (let i =0 ; i < mails.length; i++) {
+            let Elementmail=mails[i];
+            
+            if(emailIns=Elementmail){
+                output.innerHTML=("la tua e-mail già esiste!");
+                break;
+            }
+    
+            else if(!(emailIns=Elementmail)){}
+            output.innerHTML=("La tua e-mail è valida,benvenuto!");
+            break;
+        }
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
